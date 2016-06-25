31cc9506b4505f210991d6c32c21e44c319d6dcd
Cortlandt Winters <cort@cortwinters.net>
2016-06-25 15:57:08 -0400

    fix  a silly stylerule

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -11,9 +11,10 @@ module.exports = {
   ],
   // add your custom rules here
   'rules': {
+    'no-multiple-empty-lines': 'off',
     // allow paren-less arrow functions
-    'arrow-parens': 0,
+    'arrow-parens': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 'off'
   }
 }
