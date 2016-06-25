3e0dc1d64e0ed9d1d5bbdd58646cb6689353d16e
Cortlandt Winters <cort@cortwinters.net>
2016-06-25 15:56:38 -0400

    improve the layout

--- client/src/components/Player.vue
+++ client/src/components/Player.vue
@@ -1,33 +1,52 @@
 <template>
   <div class="player">
-    <p>{{ name }}</p>
+    <div class="name">
+      {{ name }}
+    </div>
     <img class="avatar" src="../assets/cav.png">
-    <p>{{ stacks }}</p>
+    <div class="bones">
+      {{ bones }}
+    </div>
   </div>
 </template>
 
 <script type="text/babel">
-	export default {
-	  data () {
-	    return {
-	      name: 'Name',
-	      stacks: 20000
-	    }
-	  }
-	}
+
+  export default {
+    data () {
+      return {
+        name: 'Name',
+        bones: 500
+      }
+    }
+  }
+
 </script>
 
 <style scoped>
 
 	.player {
+    display: inline-flex;
+    height: 150px;
+    width: 150px;
 	  color: #333333;
   	background-color: #EAEAEA;
   	background-alpa: 100%;
 	}
 
+  .name {
+    color: #315fa2;
+    background-color: #EAEAEA;
+  }
+
 	.avatar {
-		height:125px;
-		width:101px;
+		height:100px;
+		width:80px;
 		background-color: #F5F5F5;
 	}
+
+  .bones {
+    color: #878a56;
+    background-color: #EAEAEA;
+  }
 </style>
