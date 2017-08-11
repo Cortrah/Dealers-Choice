d2532d2dc3eae540d54a1607da5e7c9b1b546c9a
Cortlandt Winters <cort@cortwinters.net>
2017-08-11 04:33:00 -0400

    update pure to 1.0

--- client/src/components/Profile.vue
+++ client/src/components/Profile.vue
@@ -1,83 +1,83 @@
 <template>
     <div id="stage">
+
+        <h1>{{ title }}</h1>
+
         <form class="pure-form pure-form-stacked">
-        <fieldset>
-
-            <h1>{{ title }}</h1>
-            <div class="pure-g">
-                <div class="pure-u-1 pure-u-md-1-2">
-                    <label for="email">
-                        Email
-                    </label>
-                    <input id="email"
-                           type="email"
-                           placeholder="Email">
-
-                    <label for="password">
-                        Password
-                    </label>
-                    <input id="password"
-                           type="password"
-                           placeholder="Password">
-
-                    <label for="userName">
-                        Username
-                    </label>
-                    <input id="userName"
-                           v-model="userName"
-                           type="text"
-                           placeholder="Mina"/>
-
-                    <button @click="gotoLobby()"
-                            class="pure-button pure-button-primary">
-                        Goto the Lobby
-                    </button>
-                </div>
-
-                <div class="pure-u-1 pure-u-md-1-2">
-                     <div v-if="!botChecked" >
-                         <img class="avatar"
-                              :src="selDog"/>
-                         <select v-model="selDog">
-                             <option v-for="dog in dogs"
-                                     :value="dog.img">
-                                 {{ dog.name }}
-                             </option>
-                         </select>
-                     </div>
-                    <div v-if="botChecked">
-                        <img class="avatar"
-                             :src="selBot"/>
-                        <select v-model="selBot">
-                            <option v-for="bot in bots"
-                                    :value="bot.img">
-                                {{ bot.name }}
-                            </option>
-                        </select>
-                        <label for="ipAddress">
-                            ip:
+            <fieldset>
+                <div class="pure-g">
+                    <div class="pure-u-1 pure-u-md-1-2">
+                        <label for="email">
+                            Email
                         </label>
-                        <input id="ipAddress"
+                        <input id="email"
+                               type="email"
+                               placeholder="Email">
+
+                        <label for="password">
+                            Password
+                        </label>
+                        <input id="password"
+                               type="password"
+                               placeholder="Password">
+
+                        <label for="userName">
+                            Username
+                        </label>
+                        <input id="userName"
+                               v-model="userName"
                                type="text"
-                               placeholder="https://123.122.1.2"/>
-                        <label for="port">
-                            Port
-                        </label>
-                        <input id="port"
-                               type="text"
-                               placeholder=":8080"/>
+                               placeholder="Mina"/>
+
+                        <button @click="gotoLobby()"
+                                class="pure-button pure-button-primary">
+                            Goto the Lobby
+                        </button>
                     </div>
-                    <div>
-                        <label for="checkbox" >
-                            <input id="checkbox"
-                                   type="checkbox"
-                                   v-model="botChecked">
-                            Use a Bot
-                        </label>
+
+                    <div class="pure-u-1 pure-u-md-1-2">
+                         <div v-if="!botChecked" >
+                             <img class="avatar"
+                                  :src="selDog"/>
+                             <select v-model="selDog">
+                                 <option v-for="dog in dogs"
+                                         :value="dog.img">
+                                     {{ dog.name }}
+                                 </option>
+                             </select>
+                         </div>
+                        <div v-if="botChecked">
+                            <img class="avatar"
+                                 :src="selBot"/>
+                            <select v-model="selBot">
+                                <option v-for="bot in bots"
+                                        :value="bot.img">
+                                    {{ bot.name }}
+                                </option>
+                            </select>
+                            <label for="ipAddress">
+                                ip:
+                            </label>
+                            <input id="ipAddress"
+                                   type="text"
+                                   placeholder="https://123.122.1.2"/>
+                            <label for="port">
+                                Port
+                            </label>
+                            <input id="port"
+                                   type="text"
+                                   placeholder=":8080"/>
+                        </div>
+                        <div>
+                            <label for="checkbox" >
+                                <input id="checkbox"
+                                       type="checkbox"
+                                       v-model="botChecked">
+                                Use a Bot
+                            </label>
+                        </div>
                     </div>
                 </div>
-            </div>
-
         </fieldset>
     </form>
   </div>
